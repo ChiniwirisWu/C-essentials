0527308b0f98e12d744fef932b2723f65f6dcc19
gilbertowu <gilbertowuwu0@gmail.com>
2025-05-18 17:36:05 -0400

    updated: getInput() so it gets a whole input

--- input.h
+++ input.h
@@ -2,6 +2,8 @@
 #define INPUT
 #include <iostream>
 #include <limits>
+#include <typeinfo>
+#include <string>
 
 void clearInput();
 
@@ -12,7 +14,13 @@ T getInput(){
 
   while(isInvalid){
     std::cout << "> "; 
-    std::cin >> score;
+    //if it is a string, I need to take the whole string instead of a substring.
+    if(typeid(T).name() == typeid(std::string).name()){
+      std::getline(std::cin >> std::ws, score);
+    // else I can use the standar input.
+    } else {
+      std::cin >> score;
+    }
     if(!std::cin){
       std::cin.clear(); 
       std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
